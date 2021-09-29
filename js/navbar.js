1f172f076def311d8c70703c5fabdd66dc36b365
Isisaurus <dianavitanyi@gmail.com>
2021-09-29 17:11:38 +0200

    Keydown event listener

--- js/navbar.js
+++ js/navbar.js
@@ -4,3 +4,10 @@ const links = document.querySelector(".nav__list");
 navToggle.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
+
+// esc closes nav toggle
+window.addEventListener("keydown", function (e) {
+  if (e.keyCode === 27) {
+    links.classList.remove("show-links");
+  }
+});
