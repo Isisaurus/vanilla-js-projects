591c4fe2b9c46ca3c6d6d47595a4bce45313f0e0
Isisaurus <dianavitanyi@gmail.com>
2021-09-29 11:50:58 +0200

    Flipper btn event listener.

--- js/color-flipper.js
+++ js/color-flipper.js
@@ -1,1 +1,24 @@
-console.log("hello");
+const hex = [0, 1, 2, 3, 4, 5, 6, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("flipper-btn");
+const color = document.getElementById("flipper-span");
+
+const getRandomNum = function () {
+  return Math.floor(Math.random() * hex.length);
+};
+
+const getRandomHex = function () {
+  let hexColor = "#";
+
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNum()];
+  }
+  return hexColor;
+};
+
+btn.addEventListener("click", () => {
+  const hexColor = getRandomHex();
+
+  color.innerText = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
