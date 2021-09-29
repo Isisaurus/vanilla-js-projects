46f719538a8a0359b50df01666672c283d786a4f
Isisaurus <dianavitanyi@gmail.com>
2021-09-29 12:50:55 +0200

    Counter event listener added

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,24 @@
-console.log("hello");
+// set initial count
+let count = 0;
+
+const counter = document.getElementById("value");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("reset")) {
+      count = 0;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      return;
+    }
+    counter.innerText = count;
+  });
+});
+
+console.log(count);
