1e21441323d8cd54d562464312a7047e377b541d
Isisaurus <dianavitanyi@gmail.com>
2021-09-30 16:30:09 +0200

    Js selectors

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,8 @@
 console.log("modal");
+
+// select buttons and overlay
+const modalBtn = document.querySelector(".modal__btn");
+const closeBtn = document.querySelector(".modal__close");
+const modalOverlay = document.querySelector(".modal__overlay");
+
+// listen for clicks on modal btn and close btn
