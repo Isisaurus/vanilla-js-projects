874742c0ed84f207ec59fbe91f8c4a4670b51ec6
Isisaurus <dianavitanyi@gmail.com>
2021-09-30 18:07:04 +0200

    Event listener with quesyselector

--- js/questions.js
+++ js/questions.js
@@ -1,13 +1,22 @@
 // 1. traversing the dom
-const questionBtns = document.querySelectorAll(".btn__question");
+// const questionBtns = document.querySelectorAll(".btn__question");
 
-questionBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const question = e.currentTarget.closest(".question");
-    if (question) {
-      question.classList.toggle("show-text");
-    }
-  });
-});
+// questionBtns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const question = e.currentTarget.closest(".question");
+//     if (question) {
+//       question.classList.toggle("show-text");
+//     }
+//   });
+// });
 
 // 2. using selectors
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((q) => {
+  const btn = q.querySelector(".btn__question");
+  btn.addEventListener("click", (e) => {
+    q.classList.toggle("show-text");
+  });
+});
